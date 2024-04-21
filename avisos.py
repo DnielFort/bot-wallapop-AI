5b79647da700c4a23be5cfdc96ae3eb98fca8c2f
Daniel Fort Herasimchyk <103583534+DnielFort@users.noreply.github.com>
2024-04-21 13:57:33 +0200

    Delete avisos.py

--- avisos.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def enviar_mensaje_telegram(token, chat_id, texto):
-    url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={texto}"
-    requests.get(url)
-
